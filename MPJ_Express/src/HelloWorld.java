cfa81528329f8a7d72ef87450f22f047e9a89bf7
Emma McEvoy <62438729+EmmaM17@users.noreply.github.com>
2025-01-13 15:49:08 +0000

    Using multicore & cluster mode

---
+++ MPJ_Express/src/HelloWorld.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import mpi.* ;
+
+public class HelloWorld {
+
+    public static void main(String args[]) throws Exception {
+        MPI.Init(args) ;
+        int me = MPI.COMM_WORLD.Rank() ;
+        int size = MPI.COMM_WORLD.Size() ;
+        System.out.println("Hi from <" + me + ">") ;
+        MPI.Finalize() ;
+    }
+}
